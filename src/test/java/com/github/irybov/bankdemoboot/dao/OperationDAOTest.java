1d6382c5d80e64302cae9354ecd276ffed618dfb
Ivan <v_cho@list.ru>
2022-12-16 16:55:45 +0300

    Parametrized tests for operations

--- src/test/java/com/github/irybov/bankdemoboot/dao/OperationDAOTest.java
+++ src/test/java/com/github/irybov/bankdemoboot/dao/OperationDAOTest.java
@@ -2,20 +2,28 @@ package com.github.irybov.bankdemoboot.dao;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.stream.Stream;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import com.github.irybov.bankdemoboot.entity.Operation;
+import com.github.irybov.bankdemoboot.model.OperationPage;
 
 @Sql("/test-data-h2.sql")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -33,10 +41,35 @@ class OperationDAOTest {
 		ReflectionTestUtils.setField(operationDAO, "entityManager", entityManager);
 	}
 
-	@Test
-	void test_getAll() {
-		List<Operation> operations = operationDAO.getAll(1);
-		assertThat(operations.size()).isEqualTo(3);
+	@ParameterizedTest
+	@MethodSource("numbers")
+	void test_getAll(int id, int quantity) {
+		List<Operation> operations = operationDAO.getAll(id);
+		assertThat(operations.size()).isEqualTo(quantity);
 	}
-
+	private static Stream<Arguments> numbers() {
+		return Stream.of(Arguments.of(1, 3), Arguments.of(2, 2), Arguments.of(3, 3));
+	}
+	
+	@ParameterizedTest
+	@MethodSource("params")
+	void test_getPage(int id, String action, double minval, double maxval,
+			OffsetDateTime mindate, OffsetDateTime maxdate, int quantity) {
+		
+		OperationPage page = new OperationPage();
+		Pageable pageable = PageRequest.of(page.getPageNumber(), page.getPageSize(),
+				page.getSortDirection(), page.getSortBy());
+		
+		Page<Operation> resultPage = operationDAO.getPage(id, action, minval, maxval,
+														  mindate, maxdate, pageable);
+		
+		assertThat(resultPage.getContent().size()).isEqualTo(quantity);
+	}
+	private static Stream<Arguments> params() {
+		return Stream.of(Arguments.of(1, "deposit",  100.00, 700.00,
+				OffsetDateTime.now().minusDays(1L), OffsetDateTime.now().plusDays(1L), 1),
+				Arguments.of(2, "withdraw",  300.00, 500.00,
+						OffsetDateTime.now().minusDays(1L), OffsetDateTime.now().plusDays(1L), 0));
+	}
+	
 }
--- src/test/java/com/github/irybov/bankdemoboot/repository/OperationRepositoryTest.java
+++ src/test/java/com/github/irybov/bankdemoboot/repository/OperationRepositoryTest.java
@@ -2,15 +2,26 @@ package com.github.irybov.bankdemoboot.repository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.jdbc.Sql;
 
+import com.github.irybov.bankdemoboot.controller.dto.OperationResponseDTO;
 import com.github.irybov.bankdemoboot.entity.Operation;
+import com.github.irybov.bankdemoboot.model.OperationPage;
+import com.github.irybov.bankdemoboot.model.OperationSpecs;
 
 @Sql("/test-data-h2.sql")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -20,10 +31,40 @@ class OperationRepositoryTest {
 	@Autowired
 	private OperationRepository operationRepository;
 
-	@Test
-	void test_findBySenderOrRecipientOrderByIdDesc() {
-		List<Operation> operations = operationRepository.findBySenderOrRecipientOrderByIdDesc(1, 1);
-		assertThat(operations.size()).isEqualTo(3);
+	@ParameterizedTest
+	@MethodSource("numbers")
+	void test_findBySenderOrRecipientOrderByIdDesc(int sender, int recipient, int quantity) {
+		List<Operation> operations =
+				operationRepository.findBySenderOrRecipientOrderByIdDesc(sender, recipient);
+		assertThat(operations.size()).isEqualTo(quantity);
 	}
-
+	private static Stream<Arguments> numbers() {
+		return Stream.of(Arguments.of(1, 1, 3), Arguments.of(2, 2, 2), Arguments.of(3, 3, 3));
+	}
+	
+	@ParameterizedTest
+	@MethodSource("params")
+	void test_findAllBySpecs(int id, String action, double minval, double maxval,
+			OffsetDateTime mindate, OffsetDateTime maxdate, int quantity) {
+		
+		OperationPage page = new OperationPage();
+		Pageable pageable = PageRequest.of(page.getPageNumber(), page.getPageSize(),
+				page.getSortDirection(), page.getSortBy());
+		
+		Page<OperationResponseDTO> resultPage = operationRepository.findAll
+				(Specification.where(OperationSpecs.hasAction(action)
+				.and(OperationSpecs.hasOwner(id))
+				.and(OperationSpecs.amountBetween(minval, maxval))
+				.and(OperationSpecs.dateBetween(mindate, maxdate))), pageable)
+				.map(OperationResponseDTO::new);
+		
+		assertThat(resultPage.getContent().size()).isEqualTo(quantity);
+	}
+	private static Stream<Arguments> params() {
+		return Stream.of(Arguments.of(1, "deposit",  100.00, 700.00,
+				OffsetDateTime.now().minusDays(1L), OffsetDateTime.now().plusDays(1L), 1),
+				Arguments.of(2, "withdraw",  300.00, 500.00,
+						OffsetDateTime.now().minusDays(1L), OffsetDateTime.now().plusDays(1L), 0));
+	}
+	
 }
